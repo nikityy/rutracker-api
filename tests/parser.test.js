634dad474ee34563e153cebf5cb22365ac2ba52a
Nikita Gusarov <nickgusarov@gmail.com>
2018-02-20 17:10:42 +0300

    Rewrite tests

--- tests/parser.test.js
+++ tests/parser.test.js
@@ -2,37 +2,77 @@ const Parser = require("../lib/parser");
 const utils = require("./utils");
 
 describe('#parseSearch', () => {
-  test("returns array of search results", () => {
-    expect.assertions(3);
+  const resultsPageHtml = utils.readMockPage('search_results_page');
+  const parser = new Parser();
+  const results = parser.parseSearch(resultsPageHtml);
 
-    const resultsPageHtml = utils.readMockPage('search_results_page');
-    const parser = new Parser();
-    const results = parser.parseSearch(resultsPageHtml);
+  test("provides host", () => {
+    expect.assertions(1);
+
+    expect(results[30].host).toEqual("http://rutracker.org");
+  });
+
+  test("parses id", () => {
+    expect.assertions(1);
+
+    expect(results[30].id).toEqual("88068");
+  });
+
+  test("parses title", () => {
+    expect.assertions(1);
+
+    expect(results[30].title).toEqual("[DTSCD][DVDA] Metallica - Black Album - 2005");
+  });
+
+  test("parses category", () => {
+    expect.assertions(1);
+
+    expect(results[30].category).toEqual("Рок-музыка (многоканальная музыка)");
+  });
+
+  test("parses author", () => {
+    expect.assertions(1);
+
+    expect(results[30].author).toEqual("Меля");
+  });
+
+  test("parses author", () => {
+    expect.assertions(1);
+
+    expect(results[30].author).toEqual("Меля");
+  });
+
+  test("parses seeds", () => {
+    expect.assertions(2);
+
+    expect(results[0].seeds).toEqual(2);
+    expect(results[30].seeds).toEqual(1);
+  });
+
+  test("parses leeches", () => {
+    expect.assertions(2);
+
+    expect(results[0].leeches).toEqual(0);
+    expect(results[30].leeches).toEqual(1);
+  });
+
+  test("parses state", () => {
+    expect.assertions(1);
+
+    expect(results[0].state).toEqual("проверено");
+  });
+
+  test("parses size", () => {
+    expect.assertions(2);
+
+    expect(results[0].size).toEqual(3028992302);
+    expect(results[30].size).toEqual(682311748);
+  });
+
+  test("returns array of search results", () => {
+    expect.assertions(1);
 
     expect(results).toHaveLength(31);
-    expect(results[0]).toEqual({
-      author: "Dnipro_11",
-      category: "Зарубежная рок-музыка (оцифровки)",
-      id: "5515973",
-      leechs: "0",
-      seeds: "2",
-      size: "3.03 GB",
-      state: "проверено",
-      title:
-        "(Heavy Thrash Metal) [LP] [24/192] Metallica - Metallica (Black Album) - 27 Jul 2015, FLAC (image+.cue)",
-      url: "http://rutracker.org/forum/viewtopic.php?t=5515973"
-    });
-    expect(results[30]).toEqual({
-      author: "Меля",
-      category: "Рок-музыка (многоканальная музыка)",
-      id: "88068",
-      leechs: "1",
-      seeds: "1",
-      size: "0.68 GB",
-      state: "проверено",
-      title: "[DTSCD][DVDA] Metallica - Black Album - 2005",
-      url: "http://rutracker.org/forum/viewtopic.php?t=88068"
-    });
   });
 
   test("returns empty array if no results", () => {
